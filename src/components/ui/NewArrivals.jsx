ded5ff1d0f8ec3bd9d444f6f768205352c34b2bc
Lesuuh <lesuuh01@gmail.com>
2025-01-23 01:06:38 +0100

    refactor NewArrivals and TopSelling components to use data from db; update package.json to include json-server

--- src/components/ui/NewArrivals.jsx
+++ src/components/ui/NewArrivals.jsx
@@ -1,57 +1,7 @@
-
-import image1 from "../../assets/image 7.png";
-import image2 from "../../assets/image 7-1.png";
-import image3 from "../../assets/image 8.png";
-import image4 from "../../assets/image 8-1.png";
-import image5 from "../../assets/image 8-2.png";
-import image6 from "../../assets/image 9.png";
 import ProductCard from "./ProductCard";
+import db from "../../../db";
 
-const newArrivals = [
-  {
-    id: 1,
-    title: "T-shirt with Tape Design",
-    image: image1,
-    price: "120",
-    star: "4",
-  },
-  {
-    id: 2,
-    title: "T-shirt with Tape Design",
-    image: image2,
-    price: "150",
-    star: "5",
-  },
-
-  {
-    id: 3,
-    title: "T-shirt with Tape Design",
-    image: image3,
-    price: "120",
-    star: "4",
-  },
-  {
-    id: 4,
-    title: "T-shirt with Tape Design",
-    image: image4,
-    price: "120",
-    star: "4",
-  },
-  {
-    id: 5,
-    title: "T-shirt with Tape Design",
-    image: image5,
-    price: "120",
-    star: "4",
-  },
-  {
-    id: 6,
-    title: "T-shirt with Tape Design",
-    image: image6,
-    price: "120",
-    star: "4",
-  },
-];
+const newArrivals = db;
 
 const NewArrivals = () => {
   return (
--- src/components/ui/TopSelling.jsx
+++ src/components/ui/TopSelling.jsx
@@ -1,56 +1,8 @@
-import { FaStar } from "react-icons/fa";
-import image1 from "../../assets/image 7.png";
-import image2 from "../../assets/image 7-1.png";
-import image3 from "../../assets/image 8.png";
-import image4 from "../../assets/image 8-1.png";
-import image5 from "../../assets/image 8-2.png";
-import image6 from "../../assets/image 9.png";
+import ProductCard from "./ProductCard";
 
-const topSelling = [
-  {
-    id: 1,
-    title: "T-shirt with Tape Design",
-    image: image1,
-    price: "120",
-    star: "4",
-  },
-  {
-    id: 2,
-    title: "T-shirt with Tape Design",
-    image: image2,
-    price: "150",
-    star: "5",
-  },
+import db from "../../../db";
 
-  {
-    id: 3,
-    title: "T-shirt with Tape Design",
-    image: image3,
-    price: "120",
-    star: "4",
-  },
-  {
-    id: 4,
-    title: "T-shirt with Tape Design",
-    image: image4,
-    price: "120",
-    star: "4",
-  },
-  {
-    id: 5,
-    title: "T-shirt with Tape Design",
-    image: image5,
-    price: "120",
-    star: "4",
-  },
-  {
-    id: 6,
-    title: "T-shirt with Tape Design",
-    image: image6,
-    price: "120",
-    star: "4",
-  },
-];
+const topSelling = db;
 
 const TopSelling = () => {
   return (
@@ -58,26 +10,7 @@ const TopSelling = () => {
       <h2 className="text-center font-black text-2xl mb-2">TOP SELLINGS</h2>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center gap-4">
         {topSelling.map((item) => (
-          <div key={item.id} className="border p-2 w-full max-w-[250px] ">
-            <img
-              src={item.image}
-              alt=""
-              className="rounded-md w-full h-[60%]"
-            />
-            <div className="space-y-2">
-              <h4 className="text-sm font-semibold">{item.title}</h4>
-              <div className="flex text-sm items-center">
-                <div className="flex items-center text-yellow-300">
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                </div>
-                <p className="ml-2">{item.star}/5</p>
-              </div>
-              <h2 className="font-semibold">{`$${item.price}`}</h2>
-            </div>
-          </div>
+          <ProductCard key={item.id} item={item} />
         ))}
       </div>
       <div className="w-full flex justify-center items-center">
