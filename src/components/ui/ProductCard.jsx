b765df29b172930067a5b671330226e82803da2c
Lesuuh <lesuuh01@gmail.com>
2025-01-25 13:19:06 +0100

    enhance ProductCard with hover effects and add "Add to Cart" button

--- src/components/ui/ProductCard.jsx
+++ src/components/ui/ProductCard.jsx
@@ -8,11 +8,11 @@ const ProductCard = ({ item }) => {
   // console.log(item.id);
   return (
     <Link to={`/products/${parseInt(item.id)}`}>
-      <div key={item.id} className="border p-2 w-full max-w-[250px]">
+      <div key={item.id} className="border p-2 w-full max-w-[250px] group">
         <img
           src={`../../../images/${item.images[0]}`}
           alt={item.title}
-          className="rounded-md w-full"
+          className="rounded-md w-full transform group-hover:scale-105 transition duration-300"
         />
         <div className="space-y-2">
           <h4 className="text-sm font-semibold">{item.title}</h4>
@@ -21,6 +21,9 @@ const ProductCard = ({ item }) => {
             <p className="ml-2">{`(${item.reviews.length})`}</p>
           </div>
           <h2 className="font-semibold">{`$${calculateNewPrice(item)}`}</h2>
+          <button className="bg-black text-white text-xs w-full py-2 rounded-lg font-light hover:bg-black/80 transition duration-300">
+            Add to Cart
+          </button>
         </div>
       </div>
     </Link>
